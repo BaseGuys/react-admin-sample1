3daa33ba4c7e17c9aa9b7e3d185bc59c5a94a47e
MohammadReza Rahmatzadeh <m.rahmatzadeh@emofid.com>
2021-12-21 13:00:07 +0330

    add dashboard, add edit post, authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import * as React from "react";
 import { Admin, Resource } from 'react-admin';
 import { UserList } from './users';
+import { PostList, PostEdit, PostCreate } from './posts';
 import jsonServerProvider from 'ra-data-json-server';
+import Dashboard from "./Dashboard";
+import authProvider from './authProvider';
+
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={UserList} />
+  <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+    {/* <Resource name="posts" list={PostList} /> */}
+    {/* <Resource name="posts" list={PostList} edit={EditGuesser} /> */}
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+    <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>);
 
 export default App;
